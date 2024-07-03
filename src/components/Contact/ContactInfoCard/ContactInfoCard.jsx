59355f266419d5bf4ff846d46b6829834aa92458
PankajNarwade28 <pankajnarwade258@gmail.com>
2024-07-03 11:23:11 +0530

    update

--- src/components/Contact/ContactInfoCard/ContactInfoCard.jsx
+++ src/components/Contact/ContactInfoCard/ContactInfoCard.jsx
@@ -3,7 +3,7 @@ import "./ContactInfoCard.css";
 export const ContactInfoCard = ({ iconUrl, text, link }) => {
   return (
     <>
-      <a href={link} className="anchor" target="_blank">
+      <a href={link} className="anchor" target="_blank" rel="noreferrer">
         <div className="contactinfo">
           <div className="icon">
             <img src={iconUrl} alt="" />
