47ea08cbb777cc219ee7a71490a1cf4722d43700
PankajNarwade28 <pankajnarwade258@gmail.com>
2025-10-19 00:03:45 +0530

    fix link

--- src/util/links.js
+++ src/util/links.js
@@ -5,5 +5,6 @@ export const githubLink = "https://github.com/PankajNarwade28";
 export const linkedinLink = "https://www.linkedin.com/in/pankaj-narwade-13a053260/";
 export const mailtoLink = "mailto:pankajnarwade.work@gmail.com";
 export const username = 'Pankaj_Narwade_28'; 
+// export const username = 'atharv101'; 
 export const githubUsername = 'PankajNarwade28';
 export const leetcodelink = "https://leetcode.com/Pankaj_Narwade_28/";
