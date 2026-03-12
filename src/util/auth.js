e8fe70594a827b3a3182fcdbc04a05b41ffd03f9
PankajNarwade28 <pankajnarwade258@gmail.com>
2026-03-12 10:06:00 +0530

    fixed url

--- src/util/auth.js
+++ src/util/auth.js
@@ -9,7 +9,7 @@ export const authService = {
   // Login function
   login: async (username, password) => {
     try {
-      const response = await fetch(`${API_URL}/api/auth/login`, {
+      const response = await fetch(`${API_URL}api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
