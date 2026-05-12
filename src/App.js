9fc23e994e9a8a1ff8081317d9513cda41c5051e
PankajNarwade28 <pankajnarwade258@gmail.com>
2026-05-12 19:14:52 +0530

    added welcome toast

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { useEffect } from "react";
 import AppRoutes from "./AppRoutes";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
-import { Toaster } from "sonner";  
+import { Toaster } from "sonner";   
+import { toast } from "react-toastify";
 
 
 
 function App() {
   useEffect(() => { AOS.init(); }, []);
+  
+useEffect(() => { 
+  toast.info("Welcome to Pankaj's Portfolio!", {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+}, []);
+
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
